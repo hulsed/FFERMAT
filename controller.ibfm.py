83c1a29980a8c68d5913f72ec2f6b37b70fa50fb
hulsed <hulsed@oregonstate.edu>
2017-11-01 15:36:05 -0700

    added super simple controller model

---
+++ controller.ibfm.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  1 14:35:32 2017
+
+@author: hulsed
+"""
+
--- simple_controller.py
+++ simple_controller.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  1 14:34:50 2017
+
+@author: hulsed
+"""
+
 '''This is an example of how to represent a functional model and
 then experiment on it using IBFM
 
@@ -7,7 +14,7 @@ import ibfm
 
 if __name__ == '__main__':
 
-  eps = ibfm.Experiment('eps_small')
+  eps = ibfm.Experiment('simple_controller')
   #Run with 2 then 3 simultaneous faults
   eps.run(1)
   eps.run(2)
@@ -18,5 +25,3 @@ if __name__ == '__main__':
   #note: scenarios used are found in eps.scenarios
   #more information using eps.getScenarios()
   #end states are given by: list(map(eps.runOneScenario,eps.scenarios))
-  
-  
