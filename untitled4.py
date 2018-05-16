a74f99b9e265b23807495fcfab797831541c250a
hulsed <hulsed@oregonstate.edu>
2018-05-16 11:51:45 -0700

    created script to get variant results

--- untitled4.py
+++ untitled4.py
@@ -39,11 +39,22 @@ e5.run(1)
 
 failcost5, designcost5, utility5, scores5 = io.evaluate2(e5)
 
-failcosts=[failcost1, failcost2, failcost3, failcost4, failcost5]
-designcosts=[designcost1, designcost2, designcost3, designcost4, designcost5]
-designutils=[utility1, utility2, utility3, utility4, utility5]
+failcosts=np.array([failcost2, failcost3, failcost4, failcost5])-failcost1
+designcosts=np.array([designcost2, designcost3, designcost4, designcost5])-designcost1
+designutils=np.array([utility2, utility3, utility4, utility5])-designcost1-failcost1
 
 #noheatsource is 45
-plt.bar([0,1,2,3,4],designutils)
-plt.bar([0,1,2,3,4],failcosts)
-plt.bar([0,1,2,3,4],designcosts)
+width=0.25
+variants=np.array([1,2,3,4])
+fig, ax = plt.subplots()
+p1 = plt.bar(variants, designcosts, width, color='white', edgecolor='blue', hatch='//')
+p2 = plt.bar(variants+width ,failcosts, width, color='white', edgecolor='green', hatch='o')
+p3 = plt.bar(variants+2*width, designutils, width, color='white', edgecolor='red', hatch='x')
+
+ax.set_title('Differential Design Variant Cost')
+ax.set_xticks(variants+width)
+ax.set_xticklabels(('Variant 1', 'Variant 2', 'Variant 3', 'Variant 4'))
+ax.legend((p1[0], p2[0], p3[0]),(r'$\Delta$' ' Design Cost',r'$\Delta$' ' Failure Cost',r'$\Delta$' ' Total Cost'))
+ax.grid(axis='y', which='major')
+ax.grid(axis='y', which='minor')
+plt.minorticks_on()
