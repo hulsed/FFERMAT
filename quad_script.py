024d82280cae18f6311a1e94452684ea8f030d37
Daniel Hulse <hulsed@oregonstate.edu>
2019-08-29 16:11:09 -0700

    added plot of nominal/non-nominal flightpaths

--- quad_script.py
+++ quad_script.py
@@ -20,34 +20,48 @@ endflows, endfaults, endclass, endgraph, nomgraph, flowhist3=ffermat.proponefaul
 ffermat.showgraph(endgraph,nomgraph)
 ffermat.plotflowhist(flowhist3, 'N/A', time=0)
 
-x=flowhist3['nominal']['Env1']['x']
-y=flowhist3['nominal']['Env1']['y']
-z=flowhist3['nominal']['Env1']['elev']
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.set_xlim3d(-100, 100)
-ax.set_ylim3d(-100,100)
-ax.set_zlim3d(0,100)
-ax.plot(x,y,z)
+#x=flowhist3['nominal']['Env1']['x']
+#y=flowhist3['nominal']['Env1']['y']
+#z=flowhist3['nominal']['Env1']['elev']
+#
+#fig = plt.figure()
+#ax = fig.add_subplot(111, projection='3d')
+#ax.set_xlim3d(-100, 100)
+#ax.set_ylim3d(-100,100)
+#ax.set_zlim3d(0,100)
+#ax.plot(x,y,z)
+#plt.close()
 
 #Check various scenarios individually
 
-#endflows, endfaults, endclass, endgraph, nomgraph, flowhist=ffermat.proponefault('DistEE', 'short', mdl, time=5, track={'EE_1', 'Env1'})
-#ffermat.showgraph(endgraph,nomgraph)
+endflows, endfaults, endclass, endgraph, nomgraph, flowhist=ffermat.proponefault('DistEE', 'short', mdl, time=5, track={'EE_1', 'Env1'})
+ffermat.showgraph(endgraph,nomgraph)
 
-#ffermat.plotflowhist(flowhist, 'StoreEE short', time=5)
+ffermat.plotflowhist(flowhist, 'StoreEE short', time=5)
 
-#endflows, endfaults, endclass, endgraph, nomgraph, flowhist2=ffermat.proponefault('AffectDOF', 'RFshort', mdl, time=6, track={'DOFs', 'Env1'})
-#ffermat.showgraph(endgraph,nomgraph)
-#ffermat.plotflowhist(flowhist2, 'RFpropbreak', time=6)
+endflows, endfaults, endclass, endgraph, nomgraph, flowhist2=ffermat.proponefault('AffectDOF', 'RFshort', mdl, time=13, track={'DOFs', 'Env1'})
+ffermat.showgraph(endgraph,nomgraph)
+ffermat.plotflowhist(flowhist2, 'RFpropbreak', time=13)
 
+xnom=flowhist2['nominal']['Env1']['x']
+ynom=flowhist2['nominal']['Env1']['y']
+znom=flowhist2['nominal']['Env1']['elev']
 
+x=flowhist2['faulty']['Env1']['x']
+y=flowhist2['faulty']['Env1']['y']
+z=flowhist2['faulty']['Env1']['elev']
 
+fig2 = plt.figure(2)
+ax2 = fig2.add_subplot(111, projection='3d')
+ax2.set_xlim3d(-100, 100)
+ax2.set_ylim3d(-100,100)
+ax2.set_zlim3d(0,100)
+ax2.plot(xnom,ynom,znom)
+ax2.plot(x,y,z)
+ax2.set_title('Fault response to RFpropbreak fault at t=13')
+ax2.legend(['Nominal Flightpath','Faulty Flighpath'], loc=4)
 
-
-
-
-
+plt.show()
+plt.close()
 
 #fullresults=ffermat.proplist(mdl)
